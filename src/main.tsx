3a34c932a10e59edb16d06b270def20e262fb9ab
Ruan Lucas <ruanlucasns97@gmail.com>
2026-08-04 19:07:55 -0300

    Mount draggable keyframe controls

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import TimelineDurationControls from './components/TimelineDurationControls';
+import TimelineKeyframeDragLayer from './components/TimelineKeyframeDragLayer';
 import './styles.css';
 import './timeline-custom.css';
+import './timeline-keyframe-drag.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
     <TimelineDurationControls />
+    <TimelineKeyframeDragLayer />
   </StrictMode>,
 );
