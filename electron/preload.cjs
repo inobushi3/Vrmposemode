668e5cf4de83350674bb3dd8b1e0b8c9523e2f9d
Ruan Lucas <ruanlucasns97@gmail.com>
2026-08-04 20:25:13 -0300

    Expose secure RTMW3D bridge

--- electron/preload.cjs
+++ electron/preload.cjs
@@ -4,4 +4,14 @@ contextBridge.exposeInMainWorld('desktop', {
   minimize: () => ipcRenderer.send('window:minimize'),
   maximize: () => ipcRenderer.send('window:maximize'),
   close: () => ipcRenderer.send('window:close'),
+  rtmw3d: {
+    status: () => ipcRenderer.invoke('rtmw3d:status'),
+    prepare: () => ipcRenderer.invoke('rtmw3d:prepare'),
+    infer: (request) => ipcRenderer.invoke('rtmw3d:infer', request),
+    onProgress: (callback) => {
+      const listener = (_event, payload) => callback(payload);
+      ipcRenderer.on('rtmw3d:progress', listener);
+      return () => ipcRenderer.removeListener('rtmw3d:progress', listener);
+    },
+  },
 });
