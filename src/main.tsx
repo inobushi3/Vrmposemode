38e18c3c53331e9d2b187540f0cb278c2e2a3942
Ruan Lucas <ruanlucasns97@gmail.com>
2026-08-04 20:00:16 -0300

    Mount reference pose studio

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,17 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import TimelineDurationControls from './components/TimelineDurationControls';
 import TimelineKeyframeDragLayer from './components/TimelineKeyframeDragLayer';
+import ReferencePoseStudio from './components/ReferencePoseStudio';
 import './styles.css';
 import './timeline-custom.css';
 import './timeline-keyframe-drag.css';
+import './reference-pose.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
     <TimelineDurationControls />
     <TimelineKeyframeDragLayer />
+    <ReferencePoseStudio />
   </StrictMode>,
 );
